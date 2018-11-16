e0083ddab9f8902c971118d59f75ef3e24fd8e45
ridwanazeez <33818404+ridwanazeez@users.noreply.github.com>
2018-11-16 12:25:16 -0400

    updated files and navbar
    downloaded bootstrap and js for use without internet connection
    
    updated navbar links (building possible directory layout structure)
    
    hid file extensions in url

--- includes/authenticate.php
+++ includes/authenticate.php
@@ -5,52 +5,50 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
   <link href="../css/css.css" rel="stylesheet">
-  <link rel="icon" type="image/png" href="../favicon.png">
-  <title>Login</title>
+  <link rel="icon" type="image/png" href="favicon.png">
 </head>
 <body>
-  <div class="container">
+
+<?php
   
-    <?php
-      require_once("session.php");
-      session_start(); 
-      login();
-      function login() {
-        if(isset($_POST["login"])) {
-          $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-          $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
-          $hashpw = md5($password);
-          $result = query("SELECT * FROM accounts WHERE username = '$username' AND password = '$hashpw' LIMIT 1");
-          if (!empty($result)) {
-            createSession($username,$hashpw);
-            $user = $result[0];
-            $_SESSION['name'] = $user['username'];
-            header("Location: ../index"); 
-            return 1;
-          }
-          else {
-            echo "<p>Error: Your username or password is incorrect. Please re-enter your credentials.</p>";
-            echo "<p><a href='login'>Try Again</a></p>";
-            return 0;
-          }
-        }
+  require_once("session.php");
+  session_start(); 
+  login();
+  function login() {
+    if(isset($_POST["login"])) {
+      $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+      $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+      $hashpw = md5($password);
+      $result = query("SELECT * FROM accounts WHERE username = '$username' AND password = '$hashpw' LIMIT 1");
+      if (!empty($result)) {
+        createSession($username,$hashpw);
+        $user = $result[0];
+        $_SESSION['name'] = $user['username'];
+        header("Location: ../index"); 
+        return 1;
       }
-
-    function query($query) {
-        require_once("get_sel_db.php");
-        $st = $db->query($query);
-        $success = $st->fetchAll(PDO::FETCH_ASSOC);
-
-        require_once("close_db.php");
-        return $success;
+      else {
+        echo "<p>Error: Your username or password is incorrect. Please re-enter your credentials.</p>";
+        echo "<p><a href='login'>Try Again</a></p>";
+        return 0;
       }
-    ?>
+    }
+  }
 
-  </div>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  function query($query) {
+    require_once("get_sel_db.php");
+    $st = $db->query($query);
+    $success = $st->fetchAll(PDO::FETCH_ASSOC);
+
+    require_once("close_db.php");
+    return $success;
+  }
+?>
+
+  <script src="../js/jquery-3.3.1.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
 </body>
-</html>
+</html> 
--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(isset($_SESSION["sessionPass"]))
             {
             
             killSession();
-            header("Location:includes/login.php");
+            header("Location:includes/login");
         } ?>
 
 <!DOCTYPE html>
@@ -25,75 +25,41 @@ if(isset($_SESSION["sessionPass"]))
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link href="../css/css.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/css.css" rel="stylesheet">
   <link rel="icon" type="image/png" href="favicon.png">
   <title>Department of Computer Science</title>
 </head>
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">
-      <img src="media/cslogo.svg" width="30" height="30" class="d-inline-block align-top" alt="Department of Computer Science Logo">
-      Department of Computer Science
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-      </ul>
-      <form method="post" class="form-inline my-2 my-lg-0 mx-2">
-        <button class="btn btn-outline-success my-2 my-sm-0" name="logout" type="submit">Login</button>
-      </form>
-      <form method="post" class="form-inline my-2 my-lg-0">
-        <button class="btn btn-outline-danger my-2 my-sm-0" name="logout" type="submit">Logout</button>
-      </form>
-    </div>
-  </nav>
+  <?php
+    include("includes/nav.php");
+  ?>
 
   <div class="container">
       
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
 </body>
 </html> 
         
 <?php }
 
-else {?>
-<script>window.alert("Hmmm Whats up doc?, Try Logging in.");</script>     
-<?php
-// header("Location:includes/login.php");
-}
+  else {?>
+    <script>window.alert("Hmmm Whats up doc?, Try Logging in.");</script> 
+    <?php
+    header("Location:includes/login");
+  }
 
 }
 
-else {?>
-
-<script>window.alert("Why are you here? Please Login.");</script> 
-<?php
-header("Location:includes/login.php");
-}
+  else {?>
+    <script>window.alert("Why are you here? Please Login.");</script> 
+    <?php
+    header("Location:includes/login");
+  }
 
 ?>
