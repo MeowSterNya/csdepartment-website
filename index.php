124d680f569c9fcfeeb5c58172275336e55a975d
ridwanazeez <33818404+ridwanazeez@users.noreply.github.com>
2018-11-17 10:24:43 -0400

    minor fix to <head>

--- index.php
+++ index.php
@@ -22,7 +22,6 @@ if(isset($_SESSION["sessionPass"]))
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="css/bootstrap.min.css" rel="stylesheet">
